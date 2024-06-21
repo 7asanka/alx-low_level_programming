5b714d9c8cf7ce469af7c88a3375cb0e19b59c33
7asanka <hassan2002_kimo@hotmail.com>
2024-06-21 10:14:21 -0700

    c-hello world task 4 fix attempt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 
-/*c program that outputs string*/
+/*c program that outputs string
+ *  main() outputs string
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
