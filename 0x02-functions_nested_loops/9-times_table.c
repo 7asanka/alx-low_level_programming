248ad29e15f6cc4e8c1606d638ecce2976449e9e
7asanka <hassan2002_kimo@hotmail.com>
2024-06-25 14:56:37 -0700

    0x02 task 9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * times_table - prints the 9 times table, starting with 0.
@@ -22,7 +23,7 @@ void times_table(void)
 			}
 
 			if (r < 10)
-				putchar(' ');
+				_putchar(' ');
 
 			printf("%d", r);
 		}
