df3a8cf4ad60fd23b68291ef58c5d9ae198c4f93
7asanka <hassan2002_kimo@hotmail.com>
2024-06-25 09:56:14 -0700

    0x02 task 1 added

---
+++ 0x02-functions_nested_loops/_alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		_putchar(abc);
+	}
+	_putchar('\n');
+}
