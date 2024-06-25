e828b6088ddacc3550a863f4dd686d8aba3cad0c
7asanka <hassan2002_kimo@hotmail.com>
2024-06-25 11:49:55 -0700

    0x02 task 4 added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks weather a char is a letter or not returns 1 if true
+ * @c: character to be checked
+ *
+ * Return: 1 if true 0 if false
+ */
+int _isalpha(int c)
+{
+	if (c > 97 && c < 122 || c > 65 && c < 91)
+		return (1);
+	else
+		return (0);
+}
