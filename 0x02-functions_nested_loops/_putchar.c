e35ef32ada0510bdefc74c2cb69365bf8c35bd10
7asanka <hassan2002_kimo@hotmail.com>
2024-06-25 08:45:02 -0700

    0x02 task 0 fix

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,5 +1,4 @@
-#ifndef main
-#define main
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -9,4 +8,6 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-#endif
+{
+	return (write(1, &c, 1));
+}
