ac835e4b0f383a576b587cdf025210cac4e77193
7asanka <hassan2002_kimo@hotmail.com>
2024-06-25 17:23:20 -0700

    0x02 task 9 test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@
 void times_table(void)
 {
 	int i, j, r;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
@@ -19,7 +19,7 @@ void times_table(void)
 			if (j < 9)
 			{
 				if (r < 100)
-					printf("%d, ",r);
+					printf("%d, ", r);
 				if (r < 10)
 					printf("%d,  ", r);
 			} else
