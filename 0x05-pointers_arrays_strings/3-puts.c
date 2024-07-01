3166f6aac5e492342c53fb8bace2860a3ad7fe7c
7asanka <hassan2002_kimo@hotmail.com>
2024-07-01 10:14:44 -0700

    0x05 task 3 added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string pointer
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i, len = _strlen(str);
+
+	for (i = 0; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
