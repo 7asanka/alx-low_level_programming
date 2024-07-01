f6ec55149b1ce3f8a4d7fb68f08e5f87e3b5a74a
7asanka <hassan2002_kimo@hotmail.com>
2024-07-01 12:47:36 -0700

    0x05 task 6 fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -16,11 +16,11 @@ void puts2(char *str)
 	{
 		len++;
 	}
-	for (i = 0; i < len, i++)
+	for (i = 0; i < len; i++)
 	{
 		if (i % 2 == 0)
 		{
-		_putchar(s[i]);
+		_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
