e53ec926a1ee52b75716e59f0c6e47b688624f9f
7asanka <hassan2002_kimo@hotmail.com>
2024-07-10 09:09:14 -0700

    0x08 task 0 fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	s++;
-	_puts_recursion(s);
+
+	_putchar(*s);
+	_puts_recursion(s++);
 }
