e4102214fc0d38564f5689c426ea30a513ab5055
7asanka <hassan2002_kimo@hotmail.com>
2024-07-12 12:03:00 -0700

    0x0A task 4 fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 	{
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (int i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
