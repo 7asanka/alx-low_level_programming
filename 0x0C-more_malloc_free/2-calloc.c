40aedb65956f4ace36494a7e8883dedf8169a06c
7asanka <hassan2002_kimo@hotmail.com>
2024-07-18 10:10:28 -0700

    0x0C task 2 fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
 	void *mem;
+	char *cptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,7 +21,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == NULL)
 		return (NULL);
 
-	char *cptr = (char *)mem;
+	cptr = (char *)mem;
+
 	for (i = 0; i < nmemb * size; i++)
 	{
 		cptr[i] = 0;
