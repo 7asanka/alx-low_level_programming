64c2c32759f536c5cf2297bb0f73abb1682f64ef
7asanka <hassan2002_kimo@hotmail.com>
2024-07-18 08:45:11 -0700

    0x0C task 1 fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
-	new_str = malloc(sizeof(*new_str) * (len1 + n) + 1);
+	new_str = malloc(sizeof(char) * (len1 + n) + 1);
 	if (new_str == NULL)
 		return (NULL);
 
