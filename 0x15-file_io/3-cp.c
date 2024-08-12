423ee024fcbb4a9321e211c675ddf8d36f1e5515
7asanka <hassan2002_kimo@hotmail.com>
2024-08-12 12:37:05 -0700

    0x15 task 3 added with unknown errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,20 +44,29 @@ int main(int argc, char *argv[])
 	rdfd = open(argv[1], O_RDONLY);
 	if (rdfd == -1)
 		print_error("Error: Can't read from %s\n", argv[1], 98);
-
 	wrfd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR
 			| S_IRGRP | S_IWGRP | S_IROTH);
 	if (wrfd == -1)
+	{
+		close(rdfd);
 		print_error("Error: Can't write to %s\n", argv[2], 99);
-
+	}
 	while ((rd = read(rdfd, buffer, 1024)) > 0)
 	{
 		wr = write(wrfd, buffer, rd);
 		if (wr == -1)
+		{
+			close(rdfd);
+			close(wrfd);
 			print_error("Error: Can't write to %s\n", argv[2], 99);
+		}
 	}
 	if (rd == -1)
+	{
+		close(rdfd);
+		close(wrfd);
 		print_error("Error: Can't read from %s\n", argv[1], 98);
+	}
 	if (close(rdfd) == -1)
 		print_error("Error: Can't close fd %s\n", argv[1], 100);
 	if (close(wrfd) == -1)
